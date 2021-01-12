64214426770cdc679262d645f3b4ef7b8b2fe734
ange47rm <ange47rm@hotmail.com>
2021-01-12 22:48:52 +0000

    Managed to convert Euros to any currency, added some basic styling

--- src/app.js
+++ src/app.js
@@ -5,18 +5,29 @@ document.addEventListener ('DOMContentLoaded', function (){
     new Vue ({
         el: '#app',                                 // (1) REFER VUE OBJECT TO #APP DIV
         data: {
-            exchangeRates: []
+            inputValue: undefined,
+            exchangeRates: [],
+            selectedCurrencyRate: undefined,
+            finalValue: undefined
         },
         
         mounted: function () {                      // (3) lifecycle HOOK - the mounted is run as soon as the Vue object is created
             this.getExchangeRates();                // this means the function getExchangeRates is run as the Vue object and exchangeRates are loaded
         },
         
+        computed: {
+            
+        },
+
         methods: {
             getExchangeRates: function (){
                 fetch('https://api.exchangeratesapi.io/latest')                 // (2) We pass in the external API endpoint to get exchange rates
                 .then ( rates => rates.json())                                  // turn the result of fetch request into .json format
                 .then ( processedData => this.exchangeRates = processedData)    // take the rates now in json format and assign them to our VUE's exchangeRates
+            },
+            convertValue: function () {
+                this.finalValue = this.selectedCurrencyRate * this.inputValue;
+                return this.finalValue
             }}
 
         }
